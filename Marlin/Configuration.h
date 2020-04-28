2f34e32878017e02fc1942ad116bb725ddc9506f
Michael Gregg <mgregg@cinchcircuits.com>
2020-04-28 02:00:12 -0700

    More changes, idk yet

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Michael Gregg, printrbot simple pro)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -732,9 +732,9 @@
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 101.02, 100.82, 398.22, 124.6 }
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 202.04, 201.64, 778.44, 249.2 }
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 202.04, 201.64, 778.44, 249.2 }
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 50.51, 50.41, 199.1, 62.3 }
-//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 404.08, 403.28, 1592.88, 124.6 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 404.08, 403.28, 1592.88, 124.6 }
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 404.08, 403.28, 1592.88, 124.6 }
 
 /**
